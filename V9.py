efa0ef2b9e2ac05edaf88451be7794694318d70d
Laythe91 <109483760+Laythe91@users.noreply.github.com>
2026-09-07 07:30:01 +0200

    é majuscule

--- V9.py
+++ V9.py
@@ -195,7 +195,7 @@ st.markdown(
             🛒 OptiMarge Food
         </div>
         <div class="optimarge-subtitle">
-            Comparateur intelligent de prix fournisseurs pour mon pecore Mathieu de la part de Laythe
+            Comparateur intelligent de prix fournisseurs pour mon PÉCORE Mathieu de la part de Laythe
         </div>
     </div>
     """,
